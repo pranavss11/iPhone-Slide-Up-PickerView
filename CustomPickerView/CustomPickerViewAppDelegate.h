434d5c17d111ef54e43fc2a57d43a08f658f562d
Pranav Shah <pranavss11@gmail.com>
2011-04-29 15:06:51 -0700

    Initial Commit

---
+++ CustomPickerView/CustomPickerViewAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  CustomPickerViewAppDelegate.h
+//  CustomPickerView
+//
+//  Created by Pranav Shah on 4/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomPickerViewAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
